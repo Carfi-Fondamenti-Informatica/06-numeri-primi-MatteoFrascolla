96129ff74747b8587a34f18b57cf050540216495
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-12-03 11:35:17 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+int primo(int a, int b) {
+    bool x=true;
+    if (b!=1){
+        if ((a % b ) != 0) {
+            b--;
+            x = primo(a, b);
+        } else {
+            x=false;
+        }
+    } else {
+        x=true;
+    }
+    return x;
+}
+ 
