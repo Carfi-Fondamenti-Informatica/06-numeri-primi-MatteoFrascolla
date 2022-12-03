27e305d7987ff56c9091c2c9adaa791a0b83a420
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-12-03 11:40:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int a=0 ;
+    cin >> a;
+    int b=(a-1);
+
+    if (primo (a, b) == true) {
+        cout << "numero primo" <<endl;
+    } else if (primo (a, b) == false) {
+        cout << "numero non primo" <<endl;
+    }
   return 0;
 }
